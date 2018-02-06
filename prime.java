4823370a0c62b5518423280262581d83761692e7
shuxinwu <swu10@stuy.edu>
2018-02-06 00:07:39 -0500

    ?

---
+++ prime.java
@@ -0,0 +1,27 @@
+public class prime{
+
+  public static boolean isPrime(int n){
+    /* loop
+    for (int i = 2; i < n; i++){
+      if (n % i == 0){
+        return false;
+      }
+    }
+    return true;
+    */
+
+
+
+  }
+
+  public static boolean isSum(){
+
+  }
+
+  public static void main(String[] args){
+    System.out.println(isPrime(47));
+    System.out.println(isPrime(48));
+    System.out.println(isPrime(49));
+    System.out.println(isPrime(1047));
+  }
+}
